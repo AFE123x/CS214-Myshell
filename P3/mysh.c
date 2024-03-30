6cf351cd80b6911608c890489058a8cf08988dd9
Shadowkj1 <keshankkj@gmail.com>
2024-03-30 15:55:36 -0400

    more basic file creation

---
+++ P3/mysh.c
@@ -0,0 +1,13 @@
+//created by Arun Felix (ajf227) and Kareem Jackson (kkj47)
+
+
+#include <stdio.h>
+
+int main (int argc, char** argv) {
+
+
+
+
+
+    return 0;
+}
---
+++ documents/notes.md
@@ -0,0 +1,3 @@
+what is a tty?:
+    "something that can accept inputs from a keyboard" - https://www.youtube.com/watch?v=SYwbEcNrcjI
+
