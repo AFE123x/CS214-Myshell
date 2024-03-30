95d55106a21ed9eb8398c0916b0a89d94478cb58
AFE123x <arunf400@gmail.com>
2024-03-30 19:29:39 -0400

    updated main function

--- P3/mysh.c
+++ P3/mysh.c
@@ -1,13 +1,26 @@
-//created by Arun Felix (ajf227) and Kareem Jackson (kkj47)
-
-
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 int main (int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
 
+    int fd = open(argv[1], O_RDONLY);
+    if (fd == -1) {
+        perror("Error opening file");
+        return 1;
+    }
 
+    if (!isatty(fd)) {
+        printf("Is a valid file\n");
+    } else {
+        printf("Is not a valid file\n");
+    }
 
-
+    close(fd);
 
     return 0;
 }
