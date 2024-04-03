e2a470a6890393665531ab61842bfd32155a91d7
Shadowkj1 <keshankkj@gmail.com>
2024-04-02 22:14:12 -0400

    Pwd added and an attempt at a makefile in creation
    Co-authored-by: AFE123x <AFE123x@users.noreply.github.com>

--- P3/mysh.c
+++ P3/mysh.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include "./parser.h"
 #include <string.h>
-#include<dirent.h>
+#include <dirent.h>
+#include "./parser.h"
+
+#define BUFFER_SIZE 1024
 
 //the big 3 directories we will use to find programs
 //should be checked sequentially for programs
@@ -15,7 +17,10 @@ char* DirectoryTwo = "/usr/bin";
 char* DirectoryThree = "/bin";
 
 
-/////////////////////////built-in commands
+/////////////////////////built-in commands that need to be implemented:
+//pwd, which, exit, cd
+
+//moves you from currrent working directory to designated path
 void cd(const char *path) {
     if (chdir(path) == -1) {
         perror("cd");
@@ -39,21 +44,46 @@ void ls() {
 
     closedir(dir);
 }
+
+//prints the working directory
+void pwd() {
+//can we use getcwd?
+
+    char buf[BUFFER_SIZE];
+
+    if (getcwd(buf, sizeof(buf)) != NULL) {
+        write(STDOUT_FILENO, buf, strlen(buf));
+        //add a new line to be extra
+        write(STDOUT_FILENO, "\n",1);
+    } else {
+        perror("getcwd() error");
+        return 1;
+    }
+
+    return 0;
+}
+
+//quits the shell program
+void shell_exit() {
+
+
+}
 ////////////////////////
 
 int main (int argc, char** argv) {
+    printf("you are in mysh");
     while(1){ 
-    struct data* capybara = capygetline(STDIN_FILENO);
-    int data = capybara->size;
-    char** joever = capybara->myarray;
-    //for(int i = 0; i < data; i++){
-    //    write(STDOUT_FILENO,joever[i],strlen(joever[i]));
-    //    write(STDOUT_FILENO,"\n",1);
-    //}
-    if(!strcmp(joever[0],"ls")) ls();
-    if(!strcmp(joever[0],"cd")) cd(joever[1]);
-    free(joever);
-    free(capybara);
+        struct data* capybara = capygetline(STDIN_FILENO);
+        int data = capybara->size;
+        char** joever = capybara->myarray;
+        //for(int i = 0; i < data; i++){
+        //    write(STDOUT_FILENO,joever[i],strlen(joever[i]));
+        //    write(STDOUT_FILENO,"\n",1);
+        //}
+        if(!strcmp(joever[0],"ls")) ls();
+        if(!strcmp(joever[0],"cd")) cd(joever[1]);
+        free(joever);
+        free(capybara);
     }
     return 0;
 }
--- P3/parser.h
+++ P3/parser.h
@@ -1,6 +1,6 @@
 #ifndef PARSER_H
 #define PARSER_H
-#include<stdlib.h>
+#include <stdlib.h>
 struct data* capygetline(int fd);
 struct data{
     char** myarray;
